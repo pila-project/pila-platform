296b87d5b3a655f3f890b2d1f46b494455ff3c5f
Jason Ford <fordjason@gmail.com>
2023-09-27 14:03:55 -0700

    Add example dashboard

--- src/assignments/from-me/teacher-to-student.vue
+++ src/assignments/from-me/teacher-to-student.vue
@@ -16,7 +16,32 @@
       <input v-model="assignment.content" />
     </div>
   </div>
-  <pre>{{ mutatedScopesInContext }}</pre>
+  <table>
+    <thead>
+      <tr>
+        <th>
+          user
+        </th>
+        <th v-for="{ name } in orderedTaskMetadata">
+          {{ name }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="assignee in assignees">
+        <td>
+          {{ assignee }}
+        </td>
+        <td v-for="task in orderedTasks">
+          <ScopeWatcher
+            v-if="assigneeTasksToScopes[assignee][task]"
+            :id="assigneeTasksToScopes[assignee][task]"
+          />
+          <span v-else> - </span>
+        </td>
+      </tr>
+    </tbody>
+  </table>
   <GroupAssigner
     :id="id"
     :groups="$store.getters['groups/groups']('class')"
@@ -28,6 +53,7 @@
   import { v4 as uuid } from 'uuid'
   import { vuePersistentComponent } from '@knowlearning/agents/vue.js'
   import GroupAssigner from '../../components/groups/assigner.vue'
+  import ScopeWatcher from '../../components/scope-watcher.vue'
 
   export default {
     props: {
@@ -35,23 +61,69 @@
     },
     components: {
       GroupAssigner,
-      vuePersistentComponent
+      vuePersistentComponent,
+      ScopeWatcher
     },
     data() {
       return {
         loading: true,
         selectedFile: 'UNSELECTED',
         assignment: null,
-        mutatedScopesInContext: []
+        mutatedScopesInContext: [],
+        map: null,
+        taskMetadata: {},
+        users: []
       }
     },
     async created() {
-      console.log(this.id)
       Agent
         .query('mutated-in-context', [this.id])
         .then(results => this.mutatedScopesInContext = results)
       this.assignment = await Agent.state(this.id)
       this.loading = false
+
+      this.map = await Agent.state(this.assignment.content)
+      Object
+        .keys(this.map.nodes)
+        .forEach(async nodeId => {
+          this.taskMetadata[nodeId] = await Agent.metadata(nodeId)
+        })
+    },
+    computed: {
+      orderedTasks() {
+        return this.map ? Object.keys(this.map.nodes) : []
+      },
+      orderedTaskMetadata() {
+        if(this.map) return (
+            this.orderedTasks.map(nodeId => this.taskMetadata[nodeId] || { name: '...' })
+        )
+        else return []
+      },
+      assignees() {
+        return Array.from(new Set(
+          this
+            .mutatedScopesInContext
+            .map(({ owner }) => owner)
+        ))
+      },
+      assigneeTasksToScopes() {
+        const assigneeTasksToScopes = {}
+        this
+          .assignees
+          .forEach(id => assigneeTasksToScopes[id] = {})
+
+        this
+          .mutatedScopesInContext
+          .forEach(({ owner, target, context }) => {
+            if (context.length === 3) {
+              const task = context[2]
+              assigneeTasksToScopes[owner][task] = target
+            }
+          })
+
+        return assigneeTasksToScopes
+      }
+
     }
   }
 
---
+++ src/components/scope-watcher.vue
@@ -0,0 +1,22 @@
+<template>
+  <pre v-if="state">{{state}}</pre>
+  <span v-else>loading...</span>
+</template>
+
+<script>
+
+  export default {
+  	props: {
+  	  id: String
+  	},
+  	data() {
+  	  return {
+  	  	state: null
+  	  }
+  	},
+  	created() {
+  	  Agent.watch(this.id, ({ state }) => this.state = state)
+  	}
+  }
+
+</script>
