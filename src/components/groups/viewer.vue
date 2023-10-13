06cb7c1261f2c5307dcb781cf226962189131f4b
Jason Ford <fordjason@gmail.com>
2023-10-12 17:07:02 -0700

    Add key to classes panel

--- src/components/groups/viewer.vue
+++ src/components/groups/viewer.vue
@@ -59,7 +59,7 @@
     </Pane>
 
 
-    <Pane v-if="current">
+    <Pane v-if="current" :key="current">
       <div style="padding: 8px;">
         <h3 style="color: #2E32DB;">CLASS DETAILS</h3>
         <div> <!-- ROW FOR NAME AND ICONS -->
