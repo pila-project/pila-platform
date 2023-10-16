e9b33a7af769d3ebd80d3f7197543d4c05f35068
Jason Ford <fordjason@gmail.com>
2023-10-16 15:19:21 -0700

    Start styling content library

--- src/components/content-library.vue
+++ src/components/content-library.vue
@@ -1,34 +1,77 @@
 <template>
-  <h1>My Content</h1>
-  <input
-    type="text"
-    v-model="contentId"
-    @keydown="showUUIDWarning = false"
-  />
-  <button @click="trackContent(contentId)">Add</button>
-  <span v-if="showUUIDWarning">
-    not a valid UUID!!!
-  </span>
-  <TaggedContent type="tracked" :tags="header_tag_types" />
-  <h1>Expert Content</h1>
-  <TaggedContent type="expert" :tags="header_tag_types" />
+  <div class="cards-wrapper">
+    <IconButton
+      icon="plus-circle"
+      @click="showAddModal = true"
+      text="Add Content"
+      background="#FFC442"
+    />
+    <span v-if="showUUIDWarning">
+      not a valid UUID!!!
+    </span>
+    <div class="card-container">
+      <div
+        v-for="id in content"
+        :key="id"
+        class="card"
+      >
+        <vueEmbedComponent
+          :id="id"
+          mode="preview"
+        />
+      </div>
+    </div>
+  </div>
+  <PILAModal
+    v-if="showAddModal"
+    @close="showAddModal = false"
+    showCloseButton
+  >
+    <template v-slot:title>Add Content By Id</template>
+    <template v-slot:body>
+      <input
+        type="text"
+        v-model="contentId"
+        @keydown="showUUIDWarning = false"
+      />
+      <button @click="trackContent(contentId)">
+        Add
+      </button>
+    </template>
+  </PILAModal>
 </template>
 
 <script>
   import TaggedContent from './tagged-content-collection.vue'
+  import IconButton from './icon-button.vue'
+  import PILAModal from './PILAModal.vue'
   import { validate as isUUID } from 'uuid'
+  import { vueScopeComponent, vueEmbedComponent } from '@knowlearning/agents/vue.js'
 
   export default {
     props: {
       header_tag_types: Array
     },
     components: {
-      TaggedContent
+      TaggedContent,
+      PILAModal,
+      IconButton,
+      vueScopeComponent,
+      vueEmbedComponent
     },
     data() {
       return {
         contentId: '',
-        showUUIDWarning: false
+        showUUIDWarning: false,
+        showAddModal: false
+      }
+    },
+    computed: {
+      content() {
+        return [
+          ...this.$store.getters['pila_tags/withTag']('expert'),
+          ...this.$store.getters['pila_tags/withTag']('tracked')
+        ]
       }
     },
     methods: {
@@ -42,3 +85,29 @@
   }
   
 </script>
+
+<style scoped>
+  .cards-wrapper
+  {
+    padding: 16px;
+  }
+  .card-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  .card {
+    border: 2px solid #ccc;
+    width: 33%;
+    max-width: 256px;
+    height: 33vw;
+    max-height: 192px;
+    border-radius: 10px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    margin: 10px;
+    max-width: 300px;
+    overflow: hidden;
+    position: relative;
+  }
+</style>
