d35d3104f00ec4fba16ef44ea1f0c7656024de28
Matthew McClain <matthewmcclain@Matthews-MacBook-Pro.local>
2023-11-16 09:50:39 -0800

    style-role-request-page(s)

--- src/components/roles/requester.vue
+++ src/components/roles/requester.vue
@@ -1,27 +1,49 @@
 <template>
-  <div v-if="requestedRole">
-    You have requested to be a {{ role }}. Please wait for admin approval.
-    <button
-      v-if="requestedRole === role"
-      @click="requestRole(null)"
+  <div class="role-requester">
+    <h2>{{ t('request-role') }}</h2>
+    <div
+      v-if="requestedRole"
+      class="requested-role"
     >
-      Undo
-    </button>
-  </div>
-  <div v-if="requestedRole !== role">
-    <button @click="requestRole(role)">
-      Request "{{ role }}" Role
-    </button>
+      <p>{{ headerText }}</p> 
+      <IconButton v-if="requestedRole === role"
+        @click="requestRole(null)"
+        :text="t('undo-request')"
+        background="orange"
+      />
+    </div>
+
+    <div v-if="requestedRole !== role">
+      <IconButton
+        @click="requestRole(role)"
+        :text="buttonText"
+        background="orange"
+      />
+    </div>
+
   </div>
 </template>
 
 <script>
-
+  import IconButton from '../icon-button.vue'
   export default {
+    name: 'role-requester',
+    components: { IconButton },
     props: {
       role: String
     },
     computed: {
+      headerText() {
+        let output = this.t('you-have-requested-the-role-of')
+        output += ' '
+        output += this.t(this.role)
+        output += '. '
+        output += this.t('please-wait-for-admin-approval')
+        return output
+      },
+      buttonText() {
+        return this.t('request-role-of') + ' "' + this.t(this.role) + '"'
+      },
       requestedRole() {
         return this.$store.getters['roles/request'](this.$store.state.user)
       },
@@ -30,10 +52,29 @@
       }
     },
     methods: {
+      t(slug) { return this.$store.getters.t(slug) },
       requestRole(role) {
         this.$store.dispatch('roles/request', role)
       }
     }
   }
-
 </script>
+
+<style scoped>
+.role-requester,
+.requested-role
+{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.role-requester h2 {
+  color: #2E32DB;
+  margin: 14px;
+}
+.role-requester p {
+  margin-bottom: 8px;
+}
+
+
+</style>
