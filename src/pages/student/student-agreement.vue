2514e14e4200973b78c5985150d2a1b78c559495
Matthew McClain <matthewmcclain@Matthews-MacBook-Pro.local>
2023-11-29 09:08:06 -0800

    scaffold agree page to modal layout per spec

--- src/pages/student/student-agreement.vue
+++ src/pages/student/student-agreement.vue
@@ -1,16 +1,33 @@
 <template>
 	<div class="student-agreement">
-		STUDENT AGREEMENT PAGE
-		<button @click="agree">Agree</button>
+		<PILAModal
+			showCloseButton
+			:closeButtonText="t('agree')"
+			@close="modalClose"
+		>
+			<template v-slot:title>
+				Todo Student Agreement Header
+			</template>
+			<template v-slot:body>
+				<div class="student-agreement-body">
+					Student Agreement Body
+				</div>
+			</template>
+		</PILAModal>
 	</div>
 </template>
 
 <script>
+import PILAModal from '../../components/PILAModal.vue'
 export default {
 	name: 'student-agreement',
+	components: { PILAModal },
 	methods: {
-		agree() {
-			this.$store.dispatch('roles/acceptStudentAgreement')
+		t(slug) { return this.$store.getters.t(slug) },
+		modalClose(e) {
+			// only accept/handle modal close from 'agree' button
+			const agreeBtn = e === 'primary-button'
+			if (agreeBtn) this.$store.dispatch('roles/acceptStudentAgreement')
 		}
 	}
 }
