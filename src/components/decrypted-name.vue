fad7f19cb9557f97f2afcd531937931bc95e8928
Jason Ford <fordjason@gmail.com>
2023-12-03 17:45:20 -0800

    Decrypt user info for environment proxy

--- src/components/decrypted-name.vue
+++ src/components/decrypted-name.vue
@@ -5,9 +5,6 @@
 </template>
 
 <script>
-  import { decrypt, generateKeyPair } from '../encryption.js'
-  import { encodeBase64, decodeBase64, encodeUTF8 } from 'tweetnacl-util'
-
   export default {
     props: {
       user: String
@@ -20,37 +17,7 @@
       }
     },
     async created() {
-      const key = localStorage.getItem(`zkek-${this.$store.state.user}`)
-      if (!key) this.setAnonymous()
-      else {
-        const encryptedUserInfo = await Agent.state('encrypted-user-info', this.user)
-        const { secretKey: mySecretKey} = await generateKeyPair(key)
-        const toTry = Object.values(encryptedUserInfo)
-        let success = false
-        while (toTry.length && !success) {
-          const { publicKey: theirPublicKey, encryptedInfo } = toTry.pop()
-          try {
-            this.info = JSON.parse(
-              encodeUTF8(
-                decrypt(
-                  mySecretKey,
-                  decodeBase64(theirPublicKey),
-                  decodeBase64(encryptedInfo)
-                )
-              )
-            )
-            success = true
-          } catch (error) { console.warn(error) }
-        }
-        if (!success) this.setAnonymous()
-      }
-    },
-    methods: {
-      setAnonymous() {
-        this.info = {
-          name: `${this.$store.getters.t('anonymous')}_${this.user.slice(0,4)}`
-        }
-      }
+      this.info = await this.$store.getters.decryptUserInfo(this.user)
     }
   }
 </script>
