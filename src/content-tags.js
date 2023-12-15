f7a65b8599d1fbeb0bb85ca8211c212111217ef0
Matthew McClain <matthewmcclain@Matthews-MacBook-Pro.local>
2023-12-15 10:28:54 -0800

    test new karel tag

--- src/content-tags.js
+++ src/content-tags.js
@@ -13,4 +13,8 @@ export default {
   'https://bettysbrain.knowlearning.systems/bb/climate-change?oecd=true&disable-guide=true': [ 'intermediate', 'causal-reasoning', 'systems-thinking', 'self-regulated-learning' ],
   'https://bettysbrain.knowlearning.systems/bb/thermoregulation?oecd=true&disable-guide=true': [ 'challenge', 'causal-reasoning', 'systems-thinking', 'self-regulated-learning' ],
   '1d77b2e0-f214-4c28-a06e-2186b7f1e0b2': [ 'game-design', 'self-regulated-learning', 'computational-problem-solving'],
+
+
+  // Karel Maps
+  '146549fd-e55a-485b-972b-77d63a20fee4' : 'computational-problem-solving' // Code Fixing
 }
