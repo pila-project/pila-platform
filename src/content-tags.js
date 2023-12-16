a41f543ebd5187897487910736975a472c2f708e
Matthew McClain <mthommcclain@gmail.com>
2023-12-16 08:11:08 -0800

    add more karel map tags

--- src/content-tags.js
+++ src/content-tags.js
@@ -12,31 +12,31 @@ export default {
   'https://bettysbrain.knowlearning.systems/bb/practice?oecd=true&disable-guide=true': [ 'beginner', 'causal-reasoning', 'systems-thinking', 'self-regulated-learning' ],
   'https://bettysbrain.knowlearning.systems/bb/climate-change?oecd=true&disable-guide=true': [ 'intermediate', 'causal-reasoning', 'systems-thinking', 'self-regulated-learning' ],
   'https://bettysbrain.knowlearning.systems/bb/thermoregulation?oecd=true&disable-guide=true': [ 'challenge', 'causal-reasoning', 'systems-thinking', 'self-regulated-learning' ],
-  '1d77b2e0-f214-4c28-a06e-2186b7f1e0b2': [ 'game-design', 'self-regulated-learning', 'computational-problem-solving'], // Candli Map
+  '1d77b2e0-f214-4c28-a06e-2186b7f1e0b2': [ 'game-design', 'self-regulated-learning', 'computational-problem-solving', 'intermediate' ], // Candli Map
 
   // Karel Maps
-  '146549fd-e55a-485b-972b-77d63a20fee4': [ 'computational-problem-solving' ], // Code Fixing
-  '1629274b-0a8e-46b6-84c4-8e89e9a17149': [ 'computational-problem-solving' ], // Karel Moves Around
-  '19fb9d01-9a18-4d58-bc81-524766f11165': [ 'computational-problem-solving' ], // Simple Repeats and…
-  '29a9f5b7-4471-4f4f-ad12-bed2820f598f': [ 'computational-problem-solving' ], // Repeat tasks (simple..
-  '2f0caa84-488c-4469-a846-0dcc285b16e2': [ 'computational-problem-solving' ], // Debug with Karel
-  '395d55ab-0338-463d-b576-99732f545131': [ 'computational-problem-solving' ], // All about stones
-  '399a193c-4452-4958-bfa1-59140687c341': [ 'computational-problem-solving' ], // Learn While Loops..
+  '146549fd-e55a-485b-972b-77d63a20fee4': [ 'computational-problem-solving', 'beginner' ], // Code Fixing
+  '1629274b-0a8e-46b6-84c4-8e89e9a17149': [ 'computational-problem-solving', 'beginner' ], // Karel Moves Around
+  '19fb9d01-9a18-4d58-bc81-524766f11165': [ 'computational-problem-solving', 'beginner' ], // Simple Repeats and…
+  '29a9f5b7-4471-4f4f-ad12-bed2820f598f': [ 'computational-problem-solving', 'beginner' ], // Repeat tasks (simple..
+  '2f0caa84-488c-4469-a846-0dcc285b16e2': [ 'computational-problem-solving', 'challenge' ], // Debug with Karel
+  '395d55ab-0338-463d-b576-99732f545131': [ 'computational-problem-solving', 'beginner' ], // All about stones
+  '399a193c-4452-4958-bfa1-59140687c341': [ 'computational-problem-solving', 'intermediate' ], // Learn While Loops..
   '41e6966d-bb12-4005-a572-fa22934fe104': [ 'computational-problem-solving', 'challenge' ], // Study Karel Map 2
-  '512ab144-fef3-4ed2-97ba-003d8c542962': [ 'computational-problem-solving' ], // Learn Conditional..
-  '5d9473e2-c369-4926-9ca7-288af05cb4a5': [ 'computational-problem-solving' ], // Karel Basics
-  '658defc2-1ef2-4f16-8077-0c783b85da06': [ 'computational-problem-solving' ], // Practicing the Basics..
-  '7bdd5c3c-0070-411b-9ce3-9778c4e9cd64': [ 'computational-problem-solving' ], // Complete Code with..
-  '7cb20c85-f619-4edc-9972-df6dbe9c6dfb': [ 'computational-problem-solving' ], // Introduction to Karel
-  '8179198d-2a54-4994-9678-91aa9ced93be': [ 'computational-problem-solving' ], // Using While
-  '8b4bcf63-2ac2-4ec6-8f92-a8a989008cff': [ 'computational-problem-solving' ], // Using Functions
-  '8cc24d07-0ef1-4e3b-a6da-7d1dd16874c4': [ 'computational-problem-solving' ], // Multi-World Tasks
-  '8e01640f-5ed1-41b2-80eb-8d1f8370fe5e': [ 'computational-problem-solving' ], // Using ‘Repeat’
-  '94055b8a-84c3-404c-a047-4a50995bc90e': [ 'computational-problem-solving' ], // Beginner’s Final Challenge
+  '512ab144-fef3-4ed2-97ba-003d8c542962': [ 'computational-problem-solving', 'intermediate' ], // Learn Conditional..
+  '5d9473e2-c369-4926-9ca7-288af05cb4a5': [ 'computational-problem-solving', 'beginner' ], // Karel Basics
+  '658defc2-1ef2-4f16-8077-0c783b85da06': [ 'computational-problem-solving', 'beginner' ], // Practicing the Basics..
+  '7bdd5c3c-0070-411b-9ce3-9778c4e9cd64': [ 'computational-problem-solving', 'intermediate' ], // Complete Code with..
+  '7cb20c85-f619-4edc-9972-df6dbe9c6dfb': [ 'computational-problem-solving', 'beginner' ], // Introduction to Karel
+  '8179198d-2a54-4994-9678-91aa9ced93be': [ 'computational-problem-solving', 'intermediate' ], // Using While
+  '8b4bcf63-2ac2-4ec6-8f92-a8a989008cff': [ 'computational-problem-solving', 'intermediate' ], // Using Functions
+  '8cc24d07-0ef1-4e3b-a6da-7d1dd16874c4': [ 'computational-problem-solving', 'intermediate' ], // Multi-World Tasks
+  '8e01640f-5ed1-41b2-80eb-8d1f8370fe5e': [ 'computational-problem-solving', 'intermediate' ], // Using ‘Repeat’
+  '94055b8a-84c3-404c-a047-4a50995bc90e': [ 'computational-problem-solving', 'beginner' ], // Beginner’s Final Challenge
   '96f9c396-c42d-4000-b1f3-7b100c64a92f': [ 'computational-problem-solving', 'intermediate' ], // Study Karel Map 1
-  'a8e761e4-c58a-42a6-b5ca-7ddd5b0cadb0': [ 'computational-problem-solving' ], // Function tasks
-  'c7473040-752c-4ad2-9bac-a29f2ae4c8c2': [ 'computational-problem-solving' ], // The Turning Point
-  'dbbe3317-24d4-4cd5-8198-ce712ec8bb8f': [ 'computational-problem-solving' ], // Learn Functions
-  'f63d6ae5-9112-4d2c-bb07-89a6382589c0': [ 'computational-problem-solving' ], // Challenge yourself!
+  'a8e761e4-c58a-42a6-b5ca-7ddd5b0cadb0': [ 'computational-problem-solving', 'intermediate' ], // Function tasks
+  'c7473040-752c-4ad2-9bac-a29f2ae4c8c2': [ 'computational-problem-solving', 'beginner' ], // The Turning Point
+  'dbbe3317-24d4-4cd5-8198-ce712ec8bb8f': [ 'computational-problem-solving', 'intermediate' ], // Learn Functions
+  'f63d6ae5-9112-4d2c-bb07-89a6382589c0': [ 'computational-problem-solving', 'challenge' ], // Challenge yourself!
 
 }
