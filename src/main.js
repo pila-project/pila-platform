aa877eb6201eee81b34b5ed6168fc6dd51792b72
Matthew McClain <mthommcclain@gmail.com>
2023-12-18 13:15:28 -0800

    remove splitpanes

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import { vuePersistentStore } from '@knowlearning/agents/vue.js'
 import storeDef from './store/index.js'
 import App from './pages/App.vue'
 
-import 'splitpanes/dist/splitpanes.css'
 import './main.css'
 
 window.Agent = browserAgent()
