152d6bc14d596339cdfa01f4f8bc9571865f9b65
Jason Ford <fordjason@gmail.com>
2024-04-02 15:42:10 -0700

    Add ability to add role by user id in admin panel

--- src/pages/admin/role-table.vue
+++ src/pages/admin/role-table.vue
@@ -14,7 +14,41 @@
       {{ data.item.contributor }}
     </template>
   </v-data-table>
-  </template>
+  <v-dialog max-width="500">
+    <template v-slot:activator="{ props: activatorProps }">
+      <v-btn v-bind="activatorProps">Add {{ props.header }}</v-btn>
+    </template>
+    <template v-slot:default="{ isActive }">
+      <v-card title="New Tag">
+        <v-card-text>
+          <v-text-field
+            autofocus
+            v-model="newRoleUser"
+            label="Name"
+            @keypress.enter="() => {
+              addTagging(newRoleUser)
+              isActive.value = false
+            }"
+          />
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            text="Add"
+            @click="() => {
+              addTagging(newRoleUser)
+              isActive.value = false
+            }"
+          />
+          <v-btn
+            text="Cancel"
+            @click="isActive.value = false"
+          />
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
 
 <script setup>
   import { ref } from 'vue'
@@ -23,16 +57,32 @@
 
   const loading = ref(true)
   const taggings = ref([])
+  const newRoleUser = ref('')
 
   const headers = [
     { key: 'target', title: 'User' },
     { key: 'contributor', title: 'Assigned By' }
   ]
 
-  Agent
-    .query('taggings-for-tag', [props.partition, props.tag], 'tags.knowlearning.systems')
-    .then(result => {
-      taggings.value = result
-      loading.value = false
-    })
+  fetchTaggings()
+
+  async function fetchTaggings() {
+    loading.value = true
+    await (
+      Agent
+        .query('taggings-for-tag', [props.partition, props.tag], 'tags.knowlearning.systems')
+        .then(result => taggings.value = result)
+    )
+    loading.value = false
+   }
+
+  async function addTagging(target) {
+    const { tag, partition } = props
+
+    const myTags  = await Agent.state('tags')
+    if (!myTags[tag]) myTags[tag] = {}
+    myTags[tag][target] = { value: true, partition }
+    fetchTaggings()
+  }
+
 </script>
