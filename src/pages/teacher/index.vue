e424c8f75266c5a424484de074759828f54fed5b
Jason Ford <fordjason@gmail.com>
2024-04-24 10:43:57 -0700

    Add toolbar

--- src/pages/teacher/index.vue
+++ src/pages/teacher/index.vue
@@ -1,50 +1,81 @@
 <template>
   <TeacherAgreement v-if="!hasTeacherAgreement" />
-  <div
+  <v-app
     class="teacher-view"
     v-else-if="$store.getters['roles/hasPermission']($store.state.user, 'teacher')"
   >
 
-    <TabMenu
-      :tabs="[
-        { name: t('my-classes'), background: '#2E9DF9', id:'classes', color: 'white' },
-        { spacer: true, width: 1 },
-        { name: t('assignments'), background: '#2E32DB', id:'assignments-from-me', color: 'white' },
-        { name: t('item-library'), background: '#1B1B83', id:'content', color: 'white' },
-        { spacer: true, width: 1 },
-        { name: t('pila-studies'), background: '#6BEAC9', id:'assignments-to-me', color: 'black', icon: '/mascotte.png' }
-      ]"
-      :current="tab"
-      @select="tab = $event"
-    />
+    <v-toolbar
+      color="primary"
+    >
+      <v-toolbar-title>
+        PILA
+      </v-toolbar-title>
+      <v-spacer />
+      <v-avatar
+        class="ms-4 me-4"
+        :image="avatarImage"
+      />
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-icon
+            v-bind="props"
+            icon="fa-solid fa-gear"
+          />
+        </template>
+        <v-list>
+          <v-list-item
+            @click="logout"
+            append-icon="fa-solid fa-arrow-right-from-bracket"
+            :title="t('log-out')"
+          >
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-toolbar>
+    <v-content>
+      <TabMenu
+        :tabs="[
+          { name: t('my-classes'), background: '#2E9DF9', id:'classes', color: 'white' },
+          { spacer: true, width: 1 },
+          { name: t('assignments'), background: '#2E32DB', id:'assignments-from-me', color: 'white' },
+          { name: t('item-library'), background: '#1B1B83', id:'content', color: 'white' },
+          { spacer: true, width: 1 },
+          { name: t('pila-studies'), background: '#6BEAC9', id:'assignments-to-me', color: 'black', icon: '/mascotte.png' }
+        ]"
+        :current="tab"
+        @select="tab = $event"
+      />
 
-    <Groups
-      v-if="tab === 'classes'"
-      type="class"
-      :possibleMembers="students"
-    />
-    <div v-if="tab === 'content'">
-      <ContentLibrary />
-    </div>
-    <AssignmentsFromMe
-      v-if="tab === 'assignments-from-me'"
-      assignable_item_type="teacher-created"
-      assignment_type="teacher-to-student"
-    />
-    <StudiesNotAvailable
-      v-if="tab === 'assignments-to-me' && hideStudies"
-    />
-    <AssignmentsToMe
-      v-else-if="tab === 'assignments-to-me'"
-      type="researcher-to-teacher"
-    />
-
-  </div>
+      <Groups
+        v-if="tab === 'classes'"
+        type="class"
+        :possibleMembers="students"
+      />
+      <div v-if="tab === 'content'">
+        <ContentLibrary />
+      </div>
+      <AssignmentsFromMe
+        v-if="tab === 'assignments-from-me'"
+        assignable_item_type="teacher-created"
+        assignment_type="teacher-to-student"
+      />
+      <StudiesNotAvailable
+        v-if="tab === 'assignments-to-me' && hideStudies"
+      />
+      <AssignmentsToMe
+        v-else-if="tab === 'assignments-to-me'"
+        type="researcher-to-teacher"
+      />
+    </v-content>
+  </v-app>
 
   <RoleRequester v-else role="teacher" />
 </template>
 
-<script>
+<script setup>
+  import { ref, computed } from 'vue'
+  import { useStore } from 'vuex'
   import TeacherAgreement from './teacher-agreement.vue'
   import RoleRequester from '../../components/roles/requester.vue'
   import Groups from '../../components/groups/viewer.vue'
@@ -54,35 +85,26 @@
   import AssignmentsFromMe from '../../assignments/from-me/all.vue'
   import StudiesNotAvailable from '../../components/studies-not-available.vue'
 
-  export default {
-    name: 'teacher',
-    components: {
-      TeacherAgreement,
-      Groups,
-      ContentLibrary,
-      RoleRequester,
-      AssignmentsFromMe,
-      TabMenu,
-      AssignmentsToMe,
-      StudiesNotAvailable
-    },
-    data() {
-      return {
-        tab: 'classes',
-        hideStudies: true // For December Demo
-      }
-    },
-    computed: {
-      hasTeacherAgreement() {
-        return this.$store.getters.hasAcceptedTeacherAgreement()
-      },
-      students() {
-        return this.$store.getters['groups/myStudents']()
-      }
-    },
-    methods: {
-      t(slug) { return this.$store.getters.t(slug) }
-    }
+  const hideStudies = true
+  const tab = ref('classes')
+  const avatarImage = ref(null)
+
+  Agent.environment().then(({ auth:{info:{picture}}}) => avatarImage.value = picture)
+
+  const store = useStore()
+
+  const hasTeacherAgreement = computed(() => {
+    return store.getters.hasAcceptedTeacherAgreement()
+  })
+
+  const students = computed(() => store.getters['groups/myStudents']())
+
+  function t(slug) {
+    return store.getters.t(slug)
+  }
+
+  function logout() {
+    Agent.logout()
   }
 </script>
 
