c7ac71f132cddb7add222577c4b351471a29c8fc
Jason Ford <fordjason@gmail.com>
2024-05-03 12:13:07 -0700

    Fix names rendering in dashboard

--- src/assignments/from-me/candli-dashboard.vue
+++ src/assignments/from-me/candli-dashboard.vue
@@ -7,16 +7,20 @@
       ]
     }"
     :id="`https://competency-dashboard.pilaproject.org/?domain=${domain}&${params}`"
+    :environmentProxy="proxyEnvironmentCall"
   />
 </template>
 
 <script setup>
+  import { useStore } from 'vuex'
   import { vueEmbedComponent } from '@knowlearning/agents/vue.js'
   import { CANDLI_SEQUENCES } from '../../constants.js'
 
   const props = defineProps({
     assignment: String
   })
+
+  const store = useStore()
 
   const { domain } = await Agent.environment()
 
@@ -28,4 +32,12 @@
     ...candliGames.map(id => ['game', id]),
     ...assignmentUsers.map(id => ['user', id])
   ])
+
+  async function proxyEnvironmentCall(user) {
+    if (user) {
+      const info = await store.getters.decryptUserInfo(user)
+      return { auth: { user, info } }
+    }
+    else return Agent.environment()
+  }
 </script>
