35ed9bd4622841538b62f8f22a5cf234abc29bee
Jason Ford <fordjason@gmail.com>
2024-05-05 17:00:22 -0700

    Proxy env call for bb dashboard to set user info

--- src/assignments/from-me/dashboard/betty-dashboard.vue
+++ src/assignments/from-me/dashboard/betty-dashboard.vue
@@ -2,13 +2,25 @@
   <vueEmbedComponent
     :id="`https://bettysbrain-dashboard.knowlearning.systems/bb-dash/${props.module}/OverviewView?oecd=true&${params}`"
     :namespace="props.assignment"
+    :environmentProxy="proxyEnvironmentCall"
   />
 </template>
 
 <script setup>
+  import { useStore } from 'vuex'
   import { vueEmbedComponent } from '@knowlearning/agents/vue.js'
+
   const props = defineProps({ assignment: String, module: String, users: Array })
 
+  const store = useStore()
   const users = store.getters['assignments/assignedStudents'](props.assignment, 'teacher-to-student')
   const params = new URLSearchParams(users.map(id => ['user', id]))
+
+  async function proxyEnvironmentCall(user) {
+    if (user) {
+      const info = await store.getters.decryptUserInfo(user)
+      return { auth: { user, info } }
+    }
+    else return Agent.environment()
+  }
 </script>
