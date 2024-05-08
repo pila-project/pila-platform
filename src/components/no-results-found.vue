42989293d4db4328dd1a3f5a3652e7260471462f
mthomasmcclain <36894807+mthomasmcclain@users.noreply.github.com>
2024-05-08 09:51:00 -0700

    Empty search display (#144)
    * no-results-found component in place
    
    * smaller icon
    
    ---------
    
    Co-authored-by: Matthew McClain <mthommcclain@gmail.com>

---
+++ src/components/no-results-found.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-container
+      class="
+        d-flex
+        flex-column
+        align-center
+        mt-16
+      "
+      fluid
+    >
+      <img
+        src="/not-found-icon.jpg"
+        style="width:100px; height: 100px;"
+      >
+      <p class="mt-4">{{ t('no-results-found-for-your-filter') }}</p>
+    </v-container>
+</template>
+
+<script setup>
+import { useStore } from 'vuex'
+const store = useStore()
+function t(slug) { return store.getters.t(slug) }
+</script>
