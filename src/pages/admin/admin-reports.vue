9b4773652bdcaa944dcc0578a4db88dacaf16aa2
Jason Ford <fordjason@gmail.com>
2024-05-29 10:40:40 -0700

    Add new report

--- src/pages/admin/admin-reports.vue
+++ src/pages/admin/admin-reports.vue
@@ -1,14 +1,25 @@
 <template>
   <v-container>
     <v-btn
+      class="ma-2"
+      :color="selected === 'assignment-report' ? 'primary' : ''"
       @click="pullReport('assignment-report')"
     >
       {{ t('assignments') }}
     </v-btn>
+    <v-btn
+      class="ma-2"
+      :color="selected === 'customised_items_report' ? 'primary' : ''"
+      @click="pullReport('customised_items_report', [], 'create.pilaproject.org')"
+    >
+      {{ t('customised-items') }}
+    </v-btn>
+
     <v-data-table
       sticky
       :loading="loading"
       :items="results"
+      :headers="headers"
       :items-per-page-text="t('items-per-page')"
       :items-per-page-options="[
         {value: 10, title: '10'},
@@ -17,7 +28,14 @@
         {value: 100, title: '100'},
         {value: -1, title: t('all')}
       ]"
-    />
+    >
+      <template v-slot:item.owner="data">
+        <DecryptedName
+          avatar
+          :user="data.item.owner"
+        />
+      </template>
+    </v-data-table>
 
   </v-container>
 </template>
@@ -25,17 +43,29 @@
 <script setup>
   import { ref } from 'vue'
   import { useStore } from 'vuex'
+  import DecryptedName from '../../components/decrypted-name.vue'
 
   const store = useStore()
 
   const results = ref([])
   const loading = ref(false)
+  const selected = ref(null)
+  const headers = ref([])
 
   function t(slug) { return store.getters.t(slug)}
 
-  async function pullReport(report) {
+  async function pullReport(query, params, domain) {
     loading.value = true
-    results.value = await Agent.query(report)
+    selected.value = query
+    headers.value = undefined
+    results.value = await Agent.query(query, params, domain)
+    if (results.value[0]) {
+      headers.value = (
+        Object
+          .keys(results.value[0])
+          .map(key => ({ key, title: t(key) }))
+      )
+    }
     loading.value = false
   }
 
