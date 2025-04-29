afad3a38ba36c042f44c493428df0a43655b3659
Matthew McClain <mthommcclain@gmail.com>
2025-04-29 11:05:56 -0700

    add missing translation slug

--- src/store/translationSlugMap.js
+++ src/store/translationSlugMap.js
@@ -232,5 +232,6 @@ export default {
     "total": "fac631d0-12f2-4430-96b7-20ec1138d6e5",
     "created-by": "fc15495e-fc19-4f7e-9d91-1cfea8d56bf2",
     "your-content": "fd643305-d167-4c97-8256-39a7e5dfc614",
-    "dashboard": "fe4c0822-02da-4ed2-8629-e1aa4db8647d"
+    "dashboard": "fe4c0822-02da-4ed2-8629-e1aa4db8647d",
+    "print-login-codes" : "828892be-b9ca-406f-8053-7ad9c7ce5492",
 }
