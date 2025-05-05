22bdb351a0ebea916929d8304fe1889cbe950564
Jason Ford <fordjason@gmail.com>
2025-05-05 14:49:31 -0700

    Filter out archived users from printout

--- src/pages/teacher/codes.vue
+++ src/pages/teacher/codes.vue
@@ -2,16 +2,21 @@
   import UserInfoCard from '../../components/user-info-card.vue'
   const users = await Agent.state('users')
 
+  const visibleUsers = Object.keys(users).filter(id => !users[id].archived)
+
   setTimeout(() => {
-    window.print()
+    if (visibleUsers.length) window.print()
     //window.close()
   }, 500)
 </script>
 
 <template>
   <div>
+    <div v-if="visibleUsers.length === 0">
+      <h3>No active users with login codes.</h3>
+    </div>
     <UserInfoCard
-      v-for="_, id in users"
+      v-for="id in visibleUsers"
       :key="id"
       :id="id"
     />
