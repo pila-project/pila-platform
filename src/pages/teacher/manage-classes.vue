86129f8e09b462c7983fed6dee3b6bf310476840
mthomasmcclain <36894807+mthomasmcclain@users.noreply.github.com>
2025-05-07 11:01:59 -0700

    Show archived students (#243)
    * componentize manage-classes to prep for show archived
    
    * componentize show-archived-toggle
    
    * show archived students conditionally
    
    * get button off rail
    
    ---------
    
    Co-authored-by: Matthew McClain <mthommcclain@gmail.com>

---
+++ src/pages/teacher/manage-classes.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="manage-classes">
+
+        <UserInfoModal
+            v-if="userModalUser"
+            :id="userModalUser"
+            @close="userModalUser = null"
+        />
+        <Groups
+            type="class"
+            :possibleMembers="students"
+            @createUser="createUserAndLaunchModal"
+            @selectUser="id => userModalUser = id"
+            :key="userModalUser || 'default' /* to ensure name load on change */"
+        />
+        <v-container>
+            <ShowArchivedToggle v-model="showArchived" />
+        </v-container>
+    </div>
+</template>
+
+<script setup>
+import Groups from '../../components/groups/viewer.vue'
+import UserInfoModal from '../../components/user-info-modal.vue'
+import ShowArchivedToggle from '../../components/show-archived-toggle.vue'
+
+import { ref, reactive, computed } from 'vue'
+import { useStore } from 'vuex'
+import { createUser } from '../../user-utils.js'
+
+const userModalUser = ref(null)
+const users = reactive({})
+const showArchived = ref(false)
+
+await new Promise(resolve => {
+    Agent.watch('users', ({ state }) => {
+        resolve()
+        Object
+        .entries(state)
+        .forEach(([key, value]) => users[key] = value)
+    })
+})
+
+const store = useStore()
+function t(slug) { return store.getters.t(slug) }
+
+
+const myPILAUsers = computed(() => Object.keys(users))
+
+const students = computed(() => [
+    ...myPILAUsers.value.filter(id => showArchived.value || !users[id]?.archived),
+    ...store
+        .getters['groups/myStudents']()
+        .filter(id => !myPILAUsers.value.includes(id))
+])
+
+const codeCharacterSet = 'abcdefghijklmnopqrstuvwxy'
+
+function randomString(length, chars) {
+    const arr = new Uint8Array(length)
+    crypto.getRandomValues(arr)
+    return [...arr].map(i => chars[i % chars.length]).join('')
+}
+
+async function createUserAndLaunchModal() {
+    const providerSecret = localStorage.getItem(`zkek-${store.state.user}`)
+    const userSecret = randomString(8, codeCharacterSet)
+    const info = { name: t('student') }
+    const id = await createUser(userSecret, providerSecret, info)
+    const users = await Agent.state('users')
+    users[id] = {}
+    userModalUser.value = id
+}
+</script>
