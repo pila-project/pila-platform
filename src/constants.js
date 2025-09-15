c7050afdf09bcc9131395840197e1b8fde93bf77
Jason Ford <fordjason@gmail.com>
2025-09-15 15:43:23 -0700

    Add french access code

--- src/constants.js
+++ src/constants.js
@@ -92,7 +92,8 @@ export const DOMAIN_CODE_LISTS = {
   'app.pilaproject.org': [ 'ACCESS', 'PILA23', 'PILA4Learning23!' ],
   'thailand.pilaproject.org': ['THAI_ACCESS', 'thai_access'],
   'f74e9cb3-2b53-4c85-9b0c-f1d61b032b3f.localhost:9898': ['jason', 'thai_access'],
-  'polska-rct-2025.pilaproject.org': ['25-DOSTEP-RCT']
+  'polska-rct-2025.pilaproject.org': ['25-DOSTEP-RCT'],
+  'france-rct-2025.pilaproject.org': ['25-ACCES-RCT']
 }
 
 export const DOMAIN_DATA_PROTECTION_LINKS = {
