509e1beb2282cb8cb7d1b1c0ae2c4b501fed5f67
Jason Ford <fordjason@gmail.com>
2025-10-06 13:29:07 -0700

    Include tag translations set on translation domain

--- src/components/tag-translation.vue
+++ src/components/tag-translation.vue
@@ -13,8 +13,16 @@ const translation = getTranslation(props.id, language)
 if (translation) {
     displayString = translation
 } else {
-    const { name } = await Agent.state(props.id)
-    displayString = name || props.id
+    const translations = await Agent.query('translate-item', [ props.id, [ language ] ], 'translations.pilaproject.org')
+
+    //  Use any name translation that's not a fallback
+    const nameTranslation = translations.find(t => !t.is_fallback && t.path.length === 2 && t.path[1] === 'name')
+
+    if (nameTranslation) displayString = nameTranslation.value
+    else {
+      const { name } = await Agent.state(props.id)
+      displayString = name || props.id
+    }
 }
 
 function getTranslation(id, language) {
