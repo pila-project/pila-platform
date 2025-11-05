4fa1b94db58e29db1e4a442f4bb2a04cdf072221
Jason Ford <fordjason@gmail.com>
2025-11-05 15:57:27 -0800

    Modularize study environment variable proxy

--- src/pages/assignment/index.vue
+++ src/pages/assignment/index.vue
@@ -21,13 +21,7 @@ import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { useStore } from 'vuex'
 import { vueEmbedComponent } from '@knowlearning/agents/vue.js'
-import {
-  TREATMENT_TAG,
-  CONTROL_TAG,
-  OPT_OUT_TAG,
-  HOST_TO_PARTITION,
-  HOST_TO_FORCED_ASSIGNMENT_LANGUAGE
-} from '../../constants.js'
+import studyEnvironmentVariableProxy from '../../study-environment-variable-proxy.js'
 
 const route = useRoute()
 const store = useStore()
@@ -38,30 +32,7 @@ const assignment = ref(await Agent.state(id))
 const t = slug => store.getters.t(slug)
 const closeAssignment = () => Agent.close()
 
-const partition = HOST_TO_PARTITION[window.location.host]
-
-const { auth: { user } } = await Agent.environment()
-
-const [ treatmentTagging, optOutTagging ] = await Promise.all([
-  Agent.query('tagging-for-target', [partition, TREATMENT_TAG, user], 'tags.knowlearning.systems'),
-  Agent.query('tagging-for-target', [partition, OPT_OUT_TAG, user], 'tags.knowlearning.systems')
-])
-
-const isInTreatment = treatmentTagging.length > 0 && optOutTagging.length === 0
-
-async function addVariables(e) {
-  const env = await Agent.environment(e)
-  if (!env.variables) return env
-
-  const variables = {
-    ...env.variables,
-    TREATMENT: isInTreatment
-  }
-  const forcedLang = HOST_TO_FORCED_ASSIGNMENT_LANGUAGE[window.location.host]
-  if (forcedLang) variables.FORCED_LANGUAGE = forcedLang
-
-  return { ...env, variables }
-}
+const addVariables = await studyEnvironmentVariableProxy()
 
 </script>
 
