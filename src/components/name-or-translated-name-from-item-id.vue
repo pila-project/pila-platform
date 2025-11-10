6c52815ff77c7bf8d896581664322e49d65ada56
Matthew McClain <mthommcclain@gmail.com>
2025-11-10 09:48:09 -0800

    shrink content names >20 chars

--- src/components/name-or-translated-name-from-item-id.vue
+++ src/components/name-or-translated-name-from-item-id.vue
@@ -1,8 +1,9 @@
 <template>
-  <span>{{ displayString }}</span>
+  <span :style="nameStyle">{{ displayString }}</span>
 </template>
 
 <script setup>
+  import { computed } from 'vue'
   import { useStore } from 'vuex'
   import getName from '../name-and-translation-for-content.js'
 
@@ -10,4 +11,8 @@
   const store = useStore()
 
   const displayString = await getName(props.itemId, store.getters.language())
+
+  const nameStyle = computed(() => ({
+    fontSize: displayString.length > 20 ? '0.9rem' : 'inherit'
+  }))
 </script>
