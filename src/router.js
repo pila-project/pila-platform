9c82bb6ca0b41c8e52d86ae4c96b43dab1f265fb
mthomasmcclain <36894807+mthomasmcclain@users.noreply.github.com>
2025-11-17 17:56:56 -0800

    attempt fix of async teacher routes (#256)
    Co-authored-by: Matthew McClain <mthommcclain@gmail.com>

--- src/router.js
+++ src/router.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { defineComponent, h, ref, onMounted, markRaw } from 'vue'
+
 import AdminView from './pages/admin/index.vue'
 import ResearcherView from './pages/researcher/index.vue'
 import TeacherView from './pages/teacher/index.vue'
@@ -7,6 +9,45 @@ import StudentView from './pages/student/index.vue'
 import JoinTeacherView from './pages/student/join-teacher.vue'
 import AssignmentView from './pages/assignment/index.vue'
 import CandliStates from './pages/candli-states.vue'
+
+// Wrap  async components in synchronous wrapper
+function makeRouteShell(loader, { cache = true } = {}) {
+  let cached = null
+
+  return defineComponent({
+    name: 'RouteShell',
+    inheritAttrs: false,
+    setup(_, { attrs, slots }) {
+      const Inner = ref(cached)
+
+      onMounted(async () => {
+        if (!Inner.value) {
+          const mod = await loader()
+          Inner.value = markRaw(mod?.default || mod)
+          if (cache) cached = Inner.value
+        }
+      })
+
+      return () =>
+        h(
+          'div',
+          { style: { width: '100%', height: '100%' } },
+          Inner.value ? [h(Inner.value, { ...attrs }, slots)] : []
+        )
+    }
+  })
+}
+
+
+// TeacherView child routes wrapped in shells
+const ManageClasses = makeRouteShell(() => import('./pages/teacher/manage-classes.vue'))
+const AssignmentsFromMe = makeRouteShell(() => import('./assignments/from-me/all.vue'))
+const ContentLibrary = makeRouteShell(() => import('./components/content-library.vue'))
+const TeacherCreateTab = makeRouteShell(() => import('./pages/teacher/teacher-create-tab.vue'))
+const TeacherResourcesPage = makeRouteShell(() => import('./pages/teacher/resources-page.vue'))
+const TeacherTrainerPage = makeRouteShell(() => import('./pages/teacher/trainer-page.vue'))
+const BugReport = makeRouteShell(() => import('./components/bug-report.vue'))
+const TeacherStudyOptOut = makeRouteShell(() => import('./pages/teacher/study-opt-out.vue'))
 
 export default createRouter({
   history: createWebHistory(),
@@ -23,21 +64,21 @@ export default createRouter({
       path: '/teacher',
       component: TeacherView,
       children: [
-        { path: 'classes', component: () => import('./pages/teacher/manage-classes.vue') },
+        { path: 'classes', component: ManageClasses },
         {
           path: 'assignments-from-me',
-          component: () => import('./assignments/from-me/all.vue'),
+          component: AssignmentsFromMe,
           props: {
             assignable_item_type: 'teacher-created',
             assignment_type: 'teacher-to-student'
           }
         },
-        { path: 'content', component: () => import('./components/content-library.vue') },
-        { path: 'create', component: () => import('./pages/teacher/teacher-create-tab.vue') },
-        { path: 'resources', component: () => import('./pages/teacher/resources-page.vue') },
-        { path: 'trainer', component: () => import('./pages/teacher/trainer-page.vue') },
-        { path: 'support', component: () => import('./components/bug-report.vue') },
-        { path: 'opt-out', component: () => import('./pages/teacher/study-opt-out.vue') },
+        { path: 'content', component: ContentLibrary },
+        { path: 'create', component: TeacherCreateTab },
+        { path: 'resources', component: TeacherResourcesPage },
+        { path: 'trainer', component: TeacherTrainerPage },
+        { path: 'support', component: BugReport },
+        { path: 'opt-out', component: TeacherStudyOptOut },
         { path: '', redirect: 'teacher/classes' }
       ]
     },
