4de6c0f1c3507242248692aab08dd68fd3f9016c
jasonford <fordjason@gmail.com>
2026-05-01 15:10:13 -0700

    Revert "Add mathlive support for PILA content"
    This reverts commit d788590c2dc9b51c9d074eadb954646c1ca52ab1.

--- src/main.js
+++ src/main.js
@@ -8,8 +8,6 @@ import storeDef from './store/index.js'
 import App from './pages/App.vue'
 import runTests from './tests/index.js'
 
-import 'mathlive' // for math input support for RCT content
-
 import './main.css'
 
 import 'vuetify/styles'
@@ -19,8 +17,6 @@ import { aliases, fa } from 'vuetify/iconsets/fa'
 //  TODO: trim down imports
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-
-mathVirtualKeyboard.targetOrigin = '*' // for math input support for RCT content
 
 
 if (window.location.pathname === '/test') runTests()
