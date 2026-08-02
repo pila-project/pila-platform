fb48678423fb3aadfee5d94356a208f69ef9879c
Matthew McClain <mthommcclain@gmail.com>
2026-08-02 15:47:37 -0700

    improve naming

--- src/components/content-library.vue
+++ src/components/content-library.vue
@@ -7,7 +7,7 @@
   >
     <v-container>
       <TagFilters
-        v-if="showFilters"
+        v-if="showTaggingFeatures"
         v-model="selectedCompetencies"
         :partition="partition"
         :roots="competencies"
@@ -29,7 +29,7 @@
             :id="id"
             :selected="selfSelected === id"
             :removable="myContent.includes(id)"
-            :showTaggingIcon="!!taggingIconVisibility[id]"
+            :showTaggingIcon="showTaggingFeatures && !!taggingIconVisibility[id]"
             @click="() => {
               if (selfSelected === id) selfSelected = null
               else selfSelected = id
@@ -52,7 +52,7 @@
         @close="previewing = null"
       />
       <TaggingModal
-        v-if="tagging && showFilters"
+        v-if="tagging && showTaggingFeatures"
         :id="tagging"
         @close="tagging = null"
       />
@@ -94,7 +94,7 @@
   import TaggingModal from './tagging-modal.vue'
   import { MY_CONTENT_TAG, SIMPLIFIED_STUDY_DOMAINS } from '../constants.js'
 
-  const showFilters = !SIMPLIFIED_STUDY_DOMAINS.includes(window.location.host)
+  const showTaggingFeatures = !SIMPLIFIED_STUDY_DOMAINS.includes(window.location.host)
 
   const partition = store.getters.tagPartition
   const tag = '1a53db50-e248-11ee-ab5f-07f4a7408770'
