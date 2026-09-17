645a0e7fdcbbd659ea9b43c2ae2dbe13a1953698
jasonford <fordjason@gmail.com>
2026-09-16 23:00:17 -0700

    Add treatment/control variables to dashboard environment

--- src/assignments/from-me/dashboard/rct-dashboard.vue
+++ src/assignments/from-me/dashboard/rct-dashboard.vue
@@ -9,18 +9,23 @@
 <script setup>
   import { useStore } from 'vuex'
   import { vueEmbedComponent } from '@knowlearning/agents/vue.js'
+  import studyEnvironmentVariableProxy from '../../../study-environment-variable-proxy.js'
 
   const props = defineProps({ assignment: String, module: String, users: Array })
 
   const store = useStore()
   const users = store.getters['assignments/assignedStudents'](props.assignment, 'teacher-to-student')
   const params = new URLSearchParams(users.map(id => ['user', id]))
+  const varProxy = studyEnvironmentVariableProxy()
 
   async function proxyEnvironmentCall(user) {
     if (user) {
       const info = await store.getters.decryptUserInfo(user)
       return { auth: { user, info } }
     }
-    else return Agent.environment()
+    else {
+      const addVariables = await varProxy
+      return addVariables()
+    }
   }
 </script>
